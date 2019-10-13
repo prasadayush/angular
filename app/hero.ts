05d9fb5c04382de7357a1ce1e0e089f92c981e3c
Ayush <Ayush557@gmail.com>
2019-10-13 22:46:48 +0530

    'kl'

---
+++ app/hero.ts
@@ -0,0 +1,8 @@
+import { logging } from 'protractor';
+
+export class Hero {
+  id: number;
+  name: string;
+  empid: number;
+  place: string;
+}
---
+++ app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Ayush', empid:1580992, place:'Kolkata'},
+  { id: 2, name: 'Tanmay', empid:1580788,place:'Kolkata'},
+  { id: 3, name: 'Abhishek',empid:1580478,place:'Kolkata'},
+  { id: 4, name: 'Jyotirmay',empid:1580231,place:'Kolkata'},
+  { id: 5, name: 'Arnab',empid:1580848,place:'Kolkata'},
+  { id: 6, name: 'Saurav',empid:1434667,place:'Kolkata'},
+  { id: 7, name: 'Saunak',empid:1440207,place:'Kolkata'},
+  { id: 8, name: 'Himanshu',empid:1418076,place:'Kolkata'},
+  { id: 9, name: 'Manish',empid:1647912,place:'Kolkata'},
+  { id: 10, name: 'Rajiv',empid:1447584,place:'Kolkata'}
+];
